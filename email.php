ab87891d6601f5dd551f66cdb3ff3a337117f4d8
iyocmd <83869647+iyocmd@users.noreply.github.com>
2021-12-16 14:51:16 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,8 @@
+<?php
+// Author : Yans Ganz
+// Fb : Yans Ganz
+// Yt : Yans ScriptPrime
+// Wa : 08690085000
+// Ig : hamzatwenty
+$email = "yanskentang@gmail.com"; // Just Change Ur Email Here :)
+?>
